6d996ea3399dd5b839f27f691a8bfe1f34ddadaf
Ena Markovic <em42@uw.edu>
2015-11-15 15:27:18 -0800

    resolves issue #10

--- validation/test/signup-spec.js
+++ validation/test/signup-spec.js
@@ -1,9 +1,27 @@
 describe('paintberi sign-up app', function() {
     var emailInp = element(by.model('user.email'));
+    var dobInp = element(by.model('user.dob'));
 
     function addEmail(text) {
         emailInp.sendKeys(text);
     }
+
+    function validateDob(elem) {
+    	//page rendered, no alert
+    	expect(elem.isPresent()).toEqual(false);
+
+    	//valid date, no alert
+    	dobInp.sendKeys('11/15/1994');
+    	expect(elem.isPresent()).toEqual(false);
+
+    	//clear input, alert
+    	dobInp.clear();
+    	expect(elem.isPresent()).toEqual(true);
+
+    	//spaces input, alert
+    	dobInp.sendKeys('    ');
+    	expect(elem.isPresent()).toEqual(true);
+    }    
 
     beforeEach(function() {
        browser.get('http://localhost:8000');
@@ -24,5 +42,27 @@ describe('paintberi sign-up app', function() {
         addEmail('test@test.com');
         expect(emailAlertValid.isPresent()).toEqual(false);
         expect(emailAlertReqd.isPresent()).toEqual(false);
+    });
+
+    //test for required birth date
+    it('must have display warning if birth date is not present', function () {
+    	var dobReq = element(by.id('dobReqAlert'));
+
+    	validateDob(dobReq);
+
+		//type an invalid date -> no error
+    	dobInp.sendKeys('11/15/05');
+    	expect(dobReq.isPresent()).toEqual(false);
+    });
+
+    //test for 13 years or older 
+    it('must have display warning if user is not 13 or older', function () {
+    	var dobValid = element(by.id('dobValidAlert'));
+
+    	validateDob(dobValid);
+
+		//type an invalid date -> no error
+    	dobInp.sendKeys('11/15/05');
+    	expect(dobValid.isPresent()).toEqual(true);
     });
 });
