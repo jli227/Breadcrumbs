abb4a7ed4c23f1f02a9cc9db349a5c515c8a797d
Ena Markovic <em42@uw.edu>
2015-12-09 00:53:32 -0800

    accepted our 20 post limit fate

--- breadcrumbs/js/script.js
+++ breadcrumbs/js/script.js
@@ -57,38 +57,10 @@ angular.module('BreadcrumbsApp', ['ui.router', 'ui.bootstrap', 'chart.js'])
     })
     .controller('LoginController', function($scope) {
         $scope.login = function() {
-        /*
-            This part requires some customization (just for this stage of development)
-            the clientID and redirectURL are mine (ena's), yours will probs be different
-
-            1. go to https://www.instagram.com/developer/
-            2. sign in
-            3. click "Manage Clients" in the navbar on the upper right
-            4. register a client 
-            5. fill out things in 'Details' tab
-            6. go to the 'Security' tab, uncheck 'Disable implicit OAuth' **important**
-            7. register
-            8. update the variables below, 
-                maybe we should just comment out each others while we are developing?
-         */
-
             // paintberi's client stuff
-            var clientID = 'b1401358fc42419a8dfbd3ed74b69228';
-            // WebStorm
-            var redirectUrl = 'http://localhost:8000/paintberi/breadcrumbs/insta-oauth.html';
-
-            // Sublime
-            // var redirectUrl = 'http://localhost:8000/insta-oauth.html';
-
-            // ena's client stuff
-            // var clientID = 'e2fad0935d07402c9c5a68287915d997';
-            // var redirectUrl = 'http://localhost:8000/insta-oauth.html';
-
-            // vince's client stuff
-            // var clientID = 'ab1c06711b0046b995f3b42fd2ee5b33';
-            // var redirectUrl = 'http://localhost:8000/paintberi/breadcrumbs/insta-oauth.html';
-
-            var url = 'https://instagram.com/oauth/authorize/?client_id=' + 
+            var clientID = 'b1401358fc42419a8dfbd3ed74b69228',
+                redirectUrl = 'http://localhost:8000/paintberi/breadcrumbs/insta-oauth.html',
+                url = 'https://instagram.com/oauth/authorize/?client_id=' + 
                         clientID + '&redirect_uri=' + 
                         redirectUrl + '&response_type=token&scope=public_content';
 
