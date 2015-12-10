70d338530e74b2ada54d7f1dab7280a31bfe0a23
Ena Markovic <em42@uw.edu>
2015-12-09 22:45:50 -0800

    location merged into trends

--- breadcrumbs/js/script.js
+++ breadcrumbs/js/script.js
@@ -40,12 +40,7 @@ angular.module('BreadcrumbsApp', ['ui.router', 'ui.bootstrap', 'chart.js'])
                 url: '/activity',
                 templateUrl: 'views/activity.html',
                 controller: 'ActivityController'
-            })
-            .state('main.jonathan', {
-                url: '/jonathan',
-                templateUrl: 'views/jonathan.html',
-                controller: 'JController'
-            })            
+            })           
             .state('privacy', {
                 url: '/privacy',
                 templateUrl: 'views/privacy.html',
@@ -114,89 +109,77 @@ angular.module('BreadcrumbsApp', ['ui.router', 'ui.bootstrap', 'chart.js'])
         };
     })
     .controller('TrendsController', function ($scope, getUserData, $state, InstaURL) {
-        // "neater" way of creating the buckets?
-        //var likesBucket = {};
-        //for (var i = 1; i <= 24; i++) {
-        //    likesBucket[i] =  {count: 0, sum: 0, avg: 0};
-        //}
-        var likesBucket = {
-                0: {count: 0, sum: 0, avg: 0},
-                1: {count: 0, sum: 0, avg: 0},
-                2: {count: 0, sum: 0, avg: 0},
-                3: {count: 0, sum: 0, avg: 0},
-                4: {count: 0, sum: 0, avg: 0},
-                5: {count: 0, sum: 0, avg: 0},
-                6: {count: 0, sum: 0, avg: 0},
-                7: {count: 0, sum: 0, avg: 0},
-                8: {count: 0, sum: 0, avg: 0},
-                9: {count: 0, sum: 0, avg: 0},
-                10: {count: 0, sum: 0, avg: 0},
-                11: {count: 0, sum: 0, avg: 0},
-                12: {count: 0, sum: 0, avg: 0},
-                13: {count: 0, sum: 0, avg: 0},
-                14: {count: 0, sum: 0, avg: 0},
-                15: {count: 0, sum: 0, avg: 0},
-                16: {count: 0, sum: 0, avg: 0},
-                17: {count: 0, sum: 0, avg: 0},
-                18: {count: 0, sum: 0, avg: 0},
-                19: {count: 0, sum: 0, avg: 0},
-                20: {count: 0, sum: 0, avg: 0},
-                21: {count: 0, sum: 0, avg: 0},
-                22: {count: 0, sum: 0, avg: 0},
-                23: {count: 0, sum: 0, avg: 0}
-            },
+        var likesBucket = {},
             filterBucket = {},
+            locationBuckets = {},
             getMediaUrl = InstaURL + 'media/recent/?access_token=';
+        
+        for (var i = 0; i <= 23; i++) {
+           likesBucket[i] =  {count: 0, sum: 0, avg: 0};
+        }
 
-            getUserData(getMediaUrl)
-                .then(function (response) {
-                    
-                    response.forEach(function (post) {
-                        var time = moment.unix(post.created_time), 
-                            hour = time.hour(),
-                            minute = time.minute(),
-                            likes = post.likes.count,
-                            filter = post.filter;
+        getUserData(getMediaUrl)
+            .then(function (response) {                
+                response.forEach(function (post) {
+                    var time = moment.unix(post.created_time), 
+                        hour = time.hour(),
+                        minute = time.minute(),
+                        likes = post.likes.count,
+                        filter = post.filter,
+                        location = post.location != null ? post.location : {name: "Unknown"};
 
-                        likesBucket[hour].count++;
-                        likesBucket[hour].sum += likes;
-                        likesBucket[hour].avg = likesBucket[hour].sum / likesBucket[hour].count;             
+                    likesBucket[hour].count++;
+                    likesBucket[hour].sum += likes;
+                    likesBucket[hour].avg = likesBucket[hour].sum / likesBucket[hour].count;             
 
-                        if (!filterBucket[filter]) {
-                            filterBucket[filter] = {};
-                            filterBucket[filter].count = 0;
-                            filterBucket[filter].sum = 0;
-                        }
-                        filterBucket[filter].count++;
-                        filterBucket[filter].sum += likes;
-                        filterBucket[filter].avg = filterBucket[filter].sum / filterBucket[filter].count;
-                    });
-
-                    $scope.likesLabels = Object.keys(likesBucket).map(function (key) {
-                        return key + ':00';
-                    });
-                    $scope.likesData = [_.pluck(likesBucket, 'avg')];
-                    $scope.likesSeries = ['Time vs. Average Likes'];
-
-                    $scope.filterLabels = Object.keys(filterBucket);
-                    $scope.filterData = [_.pluck(filterBucket, 'avg')];
-                    $scope.filterSeries = ['Filter vs. Average Likes'];
-
-                    $scope.options = {
-                        tooltipTemplate: function (label) {                            
-                            return 'Average likes : ' + round(label.value);                            
-                        }
+                    if (!filterBucket[filter]) {
+                        filterBucket[filter] = {};
+                        filterBucket[filter].count = 0;
+                        filterBucket[filter].sum = 0;
                     }
+                    filterBucket[filter].count++;
+                    filterBucket[filter].sum += likes;
+                    filterBucket[filter].avg = filterBucket[filter].sum / filterBucket[filter].count;
 
-                    $scope.filterLabels = Object.keys(filterBucket);
-                    $scope.filterData = [_.pluck(filterBucket, 'avg')];
-                    $scope.filterSeries = ['Filter vs. Average Likes'];
-                    
+                    if (!locationBuckets[location]) {
+                        locationBuckets[location.name] = {};
+                        locationBuckets[location.name].count = 0;
+                        locationBuckets[location.name].sum = 0;
+                    }
+                    locationBuckets[location.name].count++;
+                    locationBuckets[location.name].sum += post.likes.count;
+                    locationBuckets[location.name].avg = locationBuckets[location.name].sum / locationBuckets[location.name].count;
+                });
 
-                    $scope.$apply();
-                }, function (error) {
-                    console.log(error);
-                }); 
+                $scope.likesLabels = Object.keys(likesBucket).map(function (key) {
+                    return key + ':00';
+                });
+                $scope.likesData = [_.pluck(likesBucket, 'avg')];
+                $scope.likesSeries = ['Time vs. Average Likes'];
+
+                $scope.filterLabels = Object.keys(filterBucket);
+                $scope.filterData = [_.pluck(filterBucket, 'avg')];
+                $scope.filterSeries = ['Filter vs. Average Likes'];
+
+                $scope.locationLabels = Object.keys(locationBuckets);
+                $scope.locationData = [_.pluck(locationBuckets, 'avg')];
+                $scope.locationSeries = ['Location vs. Likes'];
+
+                $scope.options = {
+                    tooltipTemplate: function (label) {                            
+                        return 'Average likes : ' + round(label.value);                            
+                    }
+                }
+
+                $scope.filterLabels = Object.keys(filterBucket);
+                $scope.filterData = [_.pluck(filterBucket, 'avg')];
+                $scope.filterSeries = ['Filter vs. Average Likes'];
+                
+
+                $scope.$apply();
+            }, function (error) {
+                console.log(error);
+            }); 
 
         function round(num) {
             return Math.round(num * 100) / 100;
@@ -268,27 +251,4 @@ angular.module('BreadcrumbsApp', ['ui.router', 'ui.bootstrap', 'chart.js'])
         }, function(error) {
             console.log(error);
         });
-    })
-    .controller('JController', function ($scope, getUserData, InstaURL) {
-        var getMediaUrl = InstaURL + 'media/recent/?access_token=';
-        var locationBuckets = {};
-        getUserData(getMediaUrl)
-            .then(function(response) {
-                response.forEach(function(post) {
-                    var location = post.location  != null ? post.location : {name: "Unknown"};
-                    if (!locationBuckets[location]) {
-                        locationBuckets[location.name] = {};
-                        locationBuckets[location.name].count = 0;
-                        locationBuckets[location.name].sum = 0;
-                    }
-                    locationBuckets[location.name].count++;
-                    locationBuckets[location.name].sum += post.likes.count;
-                    locationBuckets[location.name].avg = locationBuckets[location.name].sum / locationBuckets[location.name].count;
-                });
-                $scope.locationLabels = Object.keys(locationBuckets);
-                $scope.locationData = [_.pluck(locationBuckets, 'avg')];
-                $scope.locationSeries = ['Location vs. Likes'];
-
-                $scope.$apply();
-            });
     });
